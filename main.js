bdc310f238f85225f879a39672234e97706af085
frannyy11 <81949624+frannyy11@users.noreply.github.com>
2022-10-24 19:54:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var myNumber = 5; 
+var myOtherNumber= 5;
+var myTotal= myNumber * myOtherNumber;
+
+var myName = '';
+var myCombo =  myNumber + myOtherNumber;
+var myOtherCombo = myNumber + myOtherNumber;
+var myRemainder= myNumber/5;
+
+var i = 2;
+var int= 4;
+
+var myBoolean= false;
